87a44dcf811c5d57ee977e4cbe4e05115d3fcd9b
Shubhankar Srivastava <94240750+shubhankar132@users.noreply.github.com>
2024-03-20 17:14:18 +0530

    adding Authentication
    added authentication

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -6,19 +6,78 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  VStack
+  VStack,
+  useToast
 } from '@chakra-ui/react';
+import axios from "axios";
+import { useNavigate } from "react-router";
+
+
 
 const Login = () => {
   const [show, setShow] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [loading, setLoading]=useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
   const handleClick = () => setShow(!show);
 
-  const submitHandler = () => {
+  
+
+  const submitHandler = async() => {
+    setLoading(true);
+    if(!email || !password)
+    {
+      toast({
+        title: "Fill all the fields",
+        status:"warning",
+        duration: "2000",
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false)
+      return;
     // Implement your login logic here
   };
+
+  try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        "/api/user/login",
+        { email, password },
+        config
+      );
+
+      toast({
+        title: "Login Successful",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      //setUser(data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      setLoading(false);
+      navigate("/chats");
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+  };
+
 
   const getGuestCredentials = () => {
     setEmail('guest@example.com');
@@ -56,6 +115,7 @@ const Login = () => {
         width="100%"
         style={{ marginTop: 15 }}
         onClick={submitHandler}
+        isLoading={loading}
       >
         Log In
       </Button>
